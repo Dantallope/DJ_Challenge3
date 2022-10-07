2379737adb3e91b7039a1e69d0211f06bf5150c5
Dantallope <dantebraccialini@gmail.com>
2022-10-06 22:21:10 -0600

    Added promt for user error

--- script.js
+++ script.js
@@ -13,20 +13,41 @@ function generatePassword(length) {
   var result = '';
   var characters = '';
   let qUpper = prompt("Uppercase characters? Y/N");
-  let qLower = prompt("Lowercase characters? Y/N");
-  let qNum = prompt('Numbers? Y/N');
-  let qSym = prompt('Sybmols? Y/N');
+  
   if (qUpper == 'Y') {
    characters = characters + upperls;
+  } else if (qUpper == 'N'){
+    window.prompt("There will be no uppercase characters");
+  } else{
+    window.prompt("Response not recognized");
+    return;
   }
+  let qLower = prompt("Lowercase characters? Y/N");
   if (qLower == 'Y'){
     characters = characters + lowerls;
+  } else if (qLower == 'N'){
+    window.prompt("There will be no lowercase characters");
+  } else{
+    window.prompt("Response not recognized");
+    return;
   }
+  let qNum = prompt('Numbers? Y/N');
   if(qNum == 'Y'){
     characters = characters + numls;
+  } else if (qNum == 'N'){
+    window.prompt("There will be no numbers");
+  } else{
+    window.prompt("Response not recognized");
+    return;
   }
+  let qSym = prompt('Sybmols? Y/N');
   if(qSym == 'Y'){
     characters = characters + symls;
+  } else if (qSym == 'N'){
+    window.prompt("There will be no symbols");
+  } else{
+    window.prompt("Response not recognized");
+    return;
   }
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
@@ -43,6 +64,8 @@ if(pLength >= 8 && pLength <= 128){
   var password = generatePassword(pLength);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}else{
+  prompt("Improper length")
 }
 }
 
